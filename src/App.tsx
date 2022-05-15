bb21b22c36ebef8116a6598a54c261c9e32c1940
YutaroMatsumoto <spurs03170629@gmail.com>
2022-05-15 09:46:36 +0900

    💄 ProficeContent実装

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import {
   AuthenticatedTemplate,
   UnauthenticatedTemplate,
 } from '@azure/msal-react'
-import { PageLayout } from './components/PageLayout'
+import { PageLayout } from 'src/components/PageLayout'
+import { ProfileContent } from 'src/components/ProfileContent'
+import { ProfileData } from 'src/components/ProfileData'
 
 function App() {
   return (
     <PageLayout>
       <AuthenticatedTemplate>
         <p>ログインしています！</p>
+        <ProfileContent />
       </AuthenticatedTemplate>
       <UnauthenticatedTemplate>ログインしてください！</UnauthenticatedTemplate>
     </PageLayout>
---
+++ src/components/ProfileContent.tsx
@@ -0,0 +1,72 @@
+import { useState } from 'react'
+import { useMsal } from '@azure/msal-react'
+import { loginRequest } from 'src/libs/authConfig'
+import Button from 'react-bootstrap/Button'
+import { ProfileData } from 'src/components/ProfileData'
+import { callMsGraph } from 'src/libs/graph'
+
+export function ProfileContent() {
+  const { instance, accounts, inProgress } = useMsal()
+  const [accessToken, setAccessToken] = useState<string | null>(null)
+  const [graphData, setGraphData] = useState<any>(null)
+
+  const name = accounts[0] && accounts[0].name
+
+  // function RequestAccessToken() {
+  //   const request = {
+  //     ...loginRequest,
+  //     account: accounts[0],
+  //   }
+
+  //   // Silently acquires an access token which is then attached to a request for Microsoft Graph data
+  //   instance
+  //     .acquireTokenSilent(request)
+  //     .then((response) => {
+  //       setAccessToken(response.accessToken)
+  //     })
+  //     .catch((e) => {
+  //       instance.acquireTokenPopup(request).then((response) => {
+  //         setAccessToken(response.accessToken)
+  //       })
+  //     })
+  // }
+
+  function RequestProfileData() {
+    const request = {
+      ...loginRequest,
+      account: accounts[0],
+    }
+
+    // Silently acquires an access token which is then attached to a request for Microsoft Graph data
+    instance
+      .acquireTokenSilent(request)
+      .then((response) => {
+        callMsGraph(response.accessToken).then((response) => {
+          console.log('response：', response)
+          setGraphData(response)
+        })
+      })
+      .catch((e) => {
+        instance.acquireTokenPopup(request).then((response) => {
+          callMsGraph(response.accessToken).then((response) =>
+            setGraphData(response)
+          )
+        })
+      })
+  }
+
+  return (
+    <>
+      <h5 className="w-80 leading-10 text-center border-2 border-orange-400 rounded-lg bg-orange-400 text-white">
+        Welcome： {name}
+      </h5>
+      {graphData ? (
+        <ProfileData graphData={graphData} />
+      ) : (
+        <Button variant="secondary" onClick={RequestProfileData}>
+          Request Profile Information
+        </Button>
+      )}
+    </>
+  )
+}
