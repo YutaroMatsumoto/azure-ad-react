28b397c26955cbb0709230ac6f79ef69e38d3061
YutaroMatsumoto <spurs03170629@gmail.com>
2022-05-15 09:45:23 +0900

    💄 ProfileData

---
+++ src/components/ProfileData.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+type Props = {
+  graphData: any
+}
+
+/**
+ * Renders information about the user obtained from Microsoft Graph
+ */
+export const ProfileData = ({ graphData }: Props) => {
+  return (
+    <div id="profile-div">
+      <p>
+        <strong>First Name: </strong> {graphData.givenName}
+      </p>
+      <p>
+        <strong>Last Name: </strong> {graphData.surname}
+      </p>
+      <p>
+        <strong>Email: </strong> {graphData.userPrincipalName}
+      </p>
+      <p>
+        <strong>Id: </strong> {graphData.id}
+      </p>
+    </div>
+  )
+}
